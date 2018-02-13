8375f1d5c1cf66709b7541944ee8b761800b984f
William Robertson <williamp.robertson@gmail.com>
2018-02-12 23:08:35 -0700

    bug testing

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const config = {
         $filter: "env",
         $meta: 'values should not end in "/"',
         production: "https://linemancheck.herokuapp.com",
-        $default: "http://127.0.0.1:8000"
+        $default: "https://linemancheck.herokuapp.com"
     },
     authAttempts: {
         forIp: 50,
